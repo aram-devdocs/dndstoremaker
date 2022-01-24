ef585e764dc4dcc09354e41755836b62b2e1fbf8
Aram <aram@claypotcreative.com>
2022-01-24 01:14:39 -0700

    isolate items and stores by user id on back end

--- pages/api/get-all-items.js
+++ pages/api/get-all-items.js
@@ -2,6 +2,8 @@ import excuteQuery, {
   selectAllFromTable,
   selectRowFromTableWithMatch,
 } from "../../middleware/database";
+
+import { getServerSideCookie } from "../../helpers/cookieHandler";
 export default async function handler(req, res) {
   // let categories = await selectAllFromTable("item");
 
@@ -12,9 +14,14 @@ export default async function handler(req, res) {
     values: [],
   });
 
-  console.log(items);
+  // console.log(items);
 
-  let custom = await selectRowFromTableWithMatch("item", "user_id", user_id);
+  let custom = await excuteQuery({
+    query: `SELECT * from item WHERE category_id='90' AND user_id='${user_id}'`,
+    values: [],
+  });
+
+  console.log(custom);
 
   let response = items.concat(custom);
   res.status(200).json(response);
--- pages/api/get-categorized-items.js
+++ pages/api/get-categorized-items.js
@@ -2,13 +2,25 @@ import {
   selectAllFromTable,
   selectRowFromTableWithMatch,
 } from "../../middleware/database";
+import { getServerSideCookie } from "../../helpers/cookieHandler";
+import excuteQuery from "../../middleware/database";
 export default async function handler(req, res) {
   let body = JSON.parse(req.body);
-  let categories = await selectRowFromTableWithMatch(
-    "item",
-    "category_id",
-    body.category_id
-  );
+  let categories;
+  if (body.category_id == "90") {
+    let user_id = await getServerSideCookie({ req, res }, "user_id");
+    user_id = user_id.user_id;
+    categories = await excuteQuery({
+      query: `SELECT * from item WHERE category_id='90' AND user_id='${user_id}'`,
+      values: [],
+    });
+  } else {
+    categories = await selectRowFromTableWithMatch(
+      "item",
+      "category_id",
+      body.category_id
+    );
+  }
 
   // console.log(categories);
   res.status(200).json(categories);
