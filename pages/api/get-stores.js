3cc06d32ed8074da776fe1bc5296513aaa3b6f13
Aram <aram@claypotcreative.com>
2022-01-24 01:26:49 -0700

    public stores fixed

--- pages/api/get-stores.js
+++ pages/api/get-stores.js
@@ -4,15 +4,16 @@ import {
 } from "../../middleware/database";
 import { getCookie, getServerSideCookie } from "./../../helpers/cookieHandler";
 export default async function handler(req, res) {
-  // let categories = await selectAllFromTable("store");
-  let user_id = await getServerSideCookie({ req, res }, "user_id");
-  user_id = user_id.user_id;
-  // console.log(user_id);
-  let categories = await selectRowFromTableWithMatch(
-    "store",
-    "user_id",
-    user_id
-  );
-  // console.log(categories);
+  let categories;
+  try {
+    let user_id = await getServerSideCookie({ req, res }, "user_id");
+    user_id = user_id.user_id;
+    // console.log(user_id);
+    categories = await selectRowFromTableWithMatch("store", "user_id", user_id);
+    // console.log(categories);
+  } catch (error) {
+    categories = await selectAllFromTable("store");
+  }
+
   res.status(200).json(categories);
 }
